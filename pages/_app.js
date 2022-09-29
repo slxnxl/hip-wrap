6f6aecaaf03008aa370036d86eaec74edfac743a
Sypkov <sypkovstepan@gmail.com>
2022-09-29 11:35:05 +0500

    add some structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+// pages/_app.js
+import { ChakraProvider } from '@chakra-ui/react'
+import Header from "../components/header";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <ChakraProvider>
+          <Header></Header>
+        <Component {...pageProps} />
+      </ChakraProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/store/index.js
@@ -0,0 +1,8 @@
+import {Flex} from "@chakra-ui/react";
+
+export default function Store() {
+    return (
+        <Flex>Store</Flex>
+    )
+}
+
