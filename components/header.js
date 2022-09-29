56e5088e8e3f1a398d44940348d46b0c8d10d1af
Sypkov <sypkovstepan@gmail.com>
2022-09-29 19:57:20 +0500

    add templates navbar

--- components/header.js
+++ components/header.js
@@ -1,8 +1,58 @@
-import {Flex} from "@chakra-ui/react";
+import {  Box,
+    Flex,
+    Avatar,
+    HStack,
+    Link,
+    IconButton,
+    Button,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuDivider,
+    useDisclosure,
+    useColorModeValue,
+    Stack} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 
 export default function Header() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     return (
-        <Flex>123</Flex>
+        <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+            <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+                <IconButton
+                    size={'md'}
+                    icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                    aria-label={'Open Menu'}
+                    display={{ md: 'none' }}
+                    onClick={isOpen ? onClose : onOpen}
+                />
+                <Flex alignItems={'center'}>
+                    <Menu>
+                        <MenuList>
+                            <MenuItem>Link 1</MenuItem>
+                            <MenuItem>Link 2</MenuItem>
+                            <MenuDivider />
+                            <MenuItem>Link 3</MenuItem>
+                        </MenuList>
+                        <MenuButton
+                            as={Button}
+                            rounded={'full'}
+                            variant={'link'}
+                            cursor={'pointer'}
+                            minW={0}>
+                            <Avatar
+                                size={'sm'}
+                                src={
+                                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                                }
+                            />
+                        </MenuButton>
+
+                    </Menu>
+                </Flex>
+            </Flex>
+        </Box>
     )
 }
 
