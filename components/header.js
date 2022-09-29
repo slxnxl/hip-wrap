a793a500bd858d0b7aa2e1ae37d9bb03b4420768
Sypkov <sypkovstepan@gmail.com>
2022-09-29 11:35:05 +0500

    add some structure

---
+++ components/header.js
@@ -0,0 +1,8 @@
+import {Flex} from "@chakra-ui/react";
+
+export default function Header() {
+    return (
+        <Flex>123</Flex>
+    )
+}
+
---
+++ pages/contacts.js
@@ -0,0 +1,8 @@
+import {Flex} from "@chakra-ui/react";
+
+export default function Contacts() {
+    return (
+        <Flex>Contacts</Flex>
+    )
+}
+
