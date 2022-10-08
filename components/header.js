07eb186c0307e500c87305fe5566bf5e23e3148c
Sypkov <sypkovstepan@gmail.com>
2022-10-08 18:13:31 +0500

    add fonts and animation logo

--- components/header.js
+++ components/header.js
@@ -1,5 +1,7 @@
-import {  Box,
+import {
+    Box,
     Flex,
+    Container,
     Avatar,
     HStack,
     Link,
@@ -13,26 +15,28 @@ import {  Box,
     MenuDivider,
     useDisclosure,
     useColorModeValue,
-    Stack} from "@chakra-ui/react";
+    Stack, Heading
+} from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import Image from 'next/image'
+import styles from "../styles/Home.module.css";
+// import '../styles/logo.css'
 
 export default function Header() {
     // const { isOpen, onOpen, onClose } = useDisclosure();
     return (
+        <Container maxW='1200'>
         <Box px={5}>
-            <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-                <Image src={"/vercel.svg"}
-                width={100} height={50}
-                       alt={"logo"}
-                />
+            <Flex h={16} alignItems={'center'} justifyContent={'space-between'} >
+                        <Heading as='h3' size='lg' fontWeight={'800'} className={styles.logo}>HIP WRAP</Heading>
                         <ButtonGroup gap='2'>
-                        <Button className={"menu-button"}>Главная</Button>
-                        <Button className={"menu-button"}>Магазин</Button>
-                        <Button className={"menu-button"}>Контакты</Button>
+                        <Button colorScheme='gray' variant='ghost' className={"menu-button"}>главная</Button>
+                        <Button colorScheme='gray' variant='ghost' className={"menu-button"}>магазин</Button>
+                        <Button colorScheme='gray' variant='ghost' className={"menu-button"}>контакты</Button>
                         </ButtonGroup>
             </Flex>
         </Box>
+        </Container>
     )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
 import '../styles/globals.css'
 // pages/_app.js
-import { ChakraProvider } from '@chakra-ui/react'
-import Header from "../components/header";
+import {ChakraProvider, Heading} from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+import Header from "../components/header"
+
+
+const theme = extendTheme({
+    fonts: {
+        body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
+        heading: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`
+    },
+    initialColorMode: 'light',
+    useSystemColorMode: false
+})
 
 function MyApp({ Component, pageProps }) {
   return (
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
           <Header></Header>
         <Component {...pageProps} />
       </ChakraProvider>
