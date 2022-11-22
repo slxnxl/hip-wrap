84c392ad1dd0f1a7ce75bbbdd9597631e626a2c5
Sypkov <sypkovstepan@gmail.com>
2022-11-22 21:29:36 +0500

    props for filter)

--- components/grid-project/blockGrid.js
+++ components/grid-project/blockGrid.js
@@ -1,37 +1,41 @@
-import {AspectRatio, Box, Text} from "@chakra-ui/react";
+import { AspectRatio, Box, Text } from "@chakra-ui/react";
 
 
-export default function BlockGrid ({project}) {
-    let flex_size = project.flex_size
-    let height_size = project.height_size
-    //TODO грузить видео 1 раз + кэшировать
-    if (project.video === true) {
-        return (<Box
-            as="video"
-            src='/test.webm'
-            objectFit='cover'
-            loop="true"
-            autoplay="true"
-            muted="true"
-            overflow='hidden'
-            flex={flex_size}
-            height={height_size}
-            mb={1}>
-        </Box>)
-    }
-    else {
-        return (
-            <Box
-                overflow='hidden'
-                backgroundImage="url('/project_2.jpg')"
-                backgroundPosition="center"
-                bgSize='cover'
-                flex={flex_size}
-                height={height_size}
-                mb={1}
-            >
-                <Text>hello</Text>
-            </Box>
-        )
-    }
+export default function BlockGrid({ project }) {
+  let flex_size = project?.flex_size
+  let height_size = project?.height_size
+  //TODO грузить видео 1 раз + кэшировать
+  if (project?.video === true) {
+    return (
+      <Box
+        as="video"
+        src='/test.webm'
+        objectFit='cover'
+        loop={true}
+        autoPlay={true}
+        muted={true}
+        overflow='hidden'
+        flex={flex_size}
+        height={height_size}
+        mb={1}>
+      </Box>
+
+
+    )
+  }
+  else {
+    return (
+      <Box
+        overflow='hidden'
+        backgroundImage="url('/project_2.jpg')"
+        backgroundPosition="center"
+        bgSize='cover'
+        flex={flex_size}
+        height={height_size}
+        mb={1}
+      >
+        <Text>hello</Text>
+      </Box>
+    )
+  }
 }
