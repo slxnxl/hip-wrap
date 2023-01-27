ff4b7a83bb41d9622b55b04f251de05bd60b7007
Sypkov <sypkovstepan@gmail.com>
2023-01-27 23:16:14 +0500

    customise 3d model

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,9 @@
 
 export default function Footer () {
+    // TODO сделать футер по макету
     return(
         <div className="footer">
-            <h2>hello@hipwrap.com</h2>
+            <h2 >hello@hipwrap.com</h2>
         </div>
     )
 }
